3e57da79240d591af6a4987e394e85c6662f7a9f
gwhitlock8 <gavinswhitlock@gmail.com>
2020-01-16 16:29:58 -0600

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,34 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params(:name,:breed,:age))
+    if @dog.valid?
+      @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def dog_params(*args)
+    params.require(:dog).permit(*args)
+  end
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
 end
